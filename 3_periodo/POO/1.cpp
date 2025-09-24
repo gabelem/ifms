3ce5989ab50069636169afc95f2326eda2e9d72b
Belem <gabelem.moraes@gmail.com>
2025-09-23 22:14:52 -0400

    Add files via upload

---
+++ 3_periodo/POO/1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Livro 
+  {
+  string titulo;
+  string autor;
+  int ano_de_publicacacao;
+
+  public:
+
+  };
+
+class Biblioteca 
+  {
+  std::vector<Livro> colecao;
+  };
+
+int main() 
+  {
+  
+  }
