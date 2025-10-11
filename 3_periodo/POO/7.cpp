7db0b7db097d8b22c35ddfe96aeea3c6f70629ba
Belem <gabelem.moraes@gmail.com>
2025-10-10 23:21:13 -0400

    Add files via upload
    é o mesmo arquivo que eu fiz na prova do dia 10/10 de POO...

---
+++ 3_periodo/POO/7.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+//POSSUI COMPOSICAO
+class Jogador
+  {
+  string nome_item, nome_arma, nickname;
+  int item_peso, quantidade_dano_arma;
+
+  public: 
+  Jogador(string ni, string na, string nn, int ip, int qda) : nome_item(ni), nome_arma(na), nickname(nn), item_peso(ip), quantidade_dano_arma(qda)
+  {
+  cout << "[Construtor] Jogador, " << "Nickname: " << nickname << ", " << "Arma: " << nome_arma << ", " << "Nome do item: " << nome_item << "\n";
+  }
+
+  ~Jogador()
+    {
+    cout << "[Destrutor] Jogador, " << "Nickname: " << nickname << ", " << "Arma: " << nome_arma << ", " << "Nome do item: " << nome_item << "\n";
+    }
+  };
+
+// POSSUI AGREGACAO
+class Inimigo
+  {
+  string nome_arma_inimigo;
+  int hp;
+	  
+  public:
+  Inimigo(string nai, int life) : nome_arma_inimigo(nai), hp(life)
+    {
+    cout << "[Construtor] Inimigo " << "Arma: " << nome_arma_inimigo << " ,HP: " << hp << "\n";
+    }
+
+    ~Inimigo()
+    {
+    cout << "[Destrutor] Inimigo " << "Arma: " << nome_arma_inimigo << " ,HP: " << hp << "\n";
+    }
+  };
+
+int main()
+  {
+  Jogador("Papel", "Espada", "Gabriel", 2, 5);
+  Inimigo("Garrafa", 10);
+
+  return 0;
+  }
